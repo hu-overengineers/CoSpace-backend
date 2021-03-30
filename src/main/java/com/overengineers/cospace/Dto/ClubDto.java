e006ba8fcba2eaca412fdbe113b18fe788fd3b45
Yusuf Keten <ketenyusuf@gmail.com>
2021-03-31 01:19:59 +0300

    Add ClubDto

---
+++ src/main/java/com/overengineers/cospace/Dto/ClubDto.java
@@ -0,0 +1,16 @@
+package com.overengineers.cospace.Dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.Size;
+
+@Getter
+@Builder
+public class ClubDto {
+    @Size(min = 1, max = 32, message = "Club name can't be more than 32 characters!")
+    public String name;
+
+    @Size(min = 1, max = 255, message = "Club description can't be more than 255 characters!")
+    public final String description;
+}
