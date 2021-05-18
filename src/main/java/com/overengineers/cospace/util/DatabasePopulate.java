0f945be3ccefbc568e21195ef643eaa0ffb03e13
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-05-18 20:42:22 +0300

    Fix database populate

--- src/main/java/com/overengineers/cospace/util/DatabasePopulate.java
+++ src/main/java/com/overengineers/cospace/util/DatabasePopulate.java
@@ -7,7 +7,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -21,29 +24,29 @@ public class DatabasePopulate {
     private final PostRepository postRepository;
 
     @Transactional
-    public void populateDatabase(){
-        List<Authority> savedAuthorities = authorityRepository.saveAll(Set.of(new Authority(null, null, "USER"), new Authority(null, null,"ADMIN")));
-        List<String> adminList = new ArrayList<>(Arrays.asList("yusuf", "cagatay", "samil","mert","selim"));
+    public void populateDatabase() {
+        List<Authority> savedAuthorities = authorityRepository.saveAll(Set.of(new Authority(null, null, "USER"), new Authority(null, null, "ADMIN")));
+        List<String> adminList = new ArrayList<>(Arrays.asList("yusuf", "cagatay", "samil", "mert", "selim"));
         List<String> adminEmailList = new ArrayList<>(Arrays.asList("ketenyusuf", "cagatayyigit3", "m.samilatesoglu", "validatedev", "selim.seker00"));
 
 
-        for(int i = 0; i < adminList.size(); i++){
+        for (int i = 0; i < adminList.size(); i++) {
             String currentUsername = adminList.get(i);
             String currentEmail = adminEmailList.get(i);
-            Member currentAdmin = new Member(currentUsername, passwordEncoder.encode("12345"),currentEmail + "@gmail.com", null, null, Set.of(savedAuthorities.get(0), savedAuthorities.get(1)), null);
+            Member currentAdmin = new Member(currentUsername, passwordEncoder.encode("12345"), currentEmail + "@gmail.com", null, null, null, Set.of(savedAuthorities.get(0), savedAuthorities.get(1)), null);
             memberRepository.save(currentAdmin);
         }
 
         int populationCount = 3;
-        for(int i = 1; i <= populationCount; i++){
+        for (int i = 1; i <= populationCount; i++) {
 
-            Member member = new Member("member" + i, passwordEncoder.encode("123456"), "member" + i + "@gmail.com", null, null, Set.of(new Authority(null, null,"USER")), null);
+            Member member = new Member("member" + i, passwordEncoder.encode("123456"), "member" + i + "@gmail.com", null, null, null, Set.of(new Authority(null, null, "USER")), null);
             memberRepository.save(member);
 
-            Club club = new Club("club" + i, "club" + i + " Details",null);
+            Club club = new Club("club" + i, "club" + i + " Details", null);
             clubRepository.save(club);
 
-            SubClub sub = new SubClub("sub" + i, "sub" + i + " Details", 0, null, null, null, null,clubRepository.findByName("club" + i).get(), null);
+            SubClub sub = new SubClub("sub" + i, "sub" + i + " Details", 0, null, null, null, null, null, null, null);
             subClubRepository.save(sub);
 
             Post post = new Post("member" + i, "Title" + i, "This is a test content for sub" + i, i, null, subClubRepository.findByName("sub" + i).get());
