fa071c734cdc8cb38cce81b77b8d7fca39e1a452
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-21 01:22:36 +0300

    Remove password from member-dto

--- src/main/java/com/overengineers/cospace/dto/MemberDTO.java
+++ src/main/java/com/overengineers/cospace/dto/MemberDTO.java
@@ -14,10 +14,6 @@ public class MemberDTO {
     @Size(min = 1, max = 32, message = "Username can't be more than 32 characters!")
     public String username;
 
-    // TODO: Remove this.
-    @Size(min = 1, max = 32, message = "Password can't be more than 32 characters!")
-    public final String password;
-
     @Size(min = 1, max = 44, message = "E-mail can't be more than 44 characters!")
     @Email(message = "Write a valid e-mail address")
     public String email;
