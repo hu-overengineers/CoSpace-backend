3aaf810cf4bd9804a1ddf01b445afdac84ad67f4
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-05-23 14:52:00 +0300

    Fake initial database data

--- src/main/java/com/overengineers/cospace/util/DatabasePopulate.java
+++ src/main/java/com/overengineers/cospace/util/DatabasePopulate.java
@@ -1,5 +1,8 @@
 package com.overengineers.cospace.util;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import com.overengineers.cospace.entity.*;
 import com.overengineers.cospace.repository.*;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +25,8 @@ public class DatabasePopulate {
     private final PostRepository postRepository;
     private final EnrollmentRepository enrollmentRepository;
     private final QuestionRepository questionRepository;
+
+    Faker faker = new Faker();
 
     @Transactional
     public void populateDatabase() {
@@ -43,34 +49,48 @@ public class DatabasePopulate {
 
 
         int populationCount = 10;
+        List<SubClub> generatedSubClubs = new ArrayList<>();
         for (int i = 1; i <= populationCount; i++) {
-            String memberUsername = "member" + i;
+            String memberUsername = faker.name().username();
             String memberPassword = passwordEncoder.encode("123456");
             String memberEmail = memberUsername + "@gmail.com";
             Member member = new Member(memberUsername, memberPassword,memberEmail,null,null,null,null,null,Set.of(userAuthority),null , null);
             memberRepository.save(member);
 
-            Club club = new Club("club" + i, "club" + i + " Details", null);
+            String clubName = String.join(" ", faker.lorem().words(faker.number().numberBetween(1, 3)));
+            Club club = new Club(clubName, faker.lorem().sentence(), null);
             clubRepository.save(club);
 
-            SubClub sub = new SubClub("sub" + i, "sub" + i + " Details", 0, null, null, null, null, club, null, null, null,null);
-            subClubRepository.save(sub);
+            for (int k = 0; k < faker.number().numberBetween(1, 5); k++) {
 
-            for(int j = 1; j <= 5; j++){
-                Question question = new Question("q" + i + j,"a1","a2","a3", "a" + i + j, "a" + i + j, subClubRepository.findByName("sub" + i).get());
-                Question savedQuestion = questionRepository.save(question);
-            }
+                SubClub sub = new SubClub(String.join(" ", faker.lorem().words(faker.number().numberBetween(1, 3))),
+                        faker.lorem().sentence(), 0, null, null, null, null, club, null, null, null,null);
+                sub = subClubRepository.save(sub);
 
-            for (int j = 0; j < 20; j++) {
-                Post post = new Post("member" + i, "Title" + i, "This is a test content for sub" + i, i, null, subClubRepository.findByName("sub" + i).get());
-                postRepository.save(post);
+                for(int j = 1; j <= 5; j++){
+                    String truth = faker.lorem().word();
+                    Question question = new Question(faker.lorem().sentence(), truth, faker.lorem().word(), faker.lorem().word(), faker.lorem().word(),
+                            truth, sub);
+                    Question savedQuestion = questionRepository.save(question);
+                }
+
+                for (int j = 0; j < faker.number().numberBetween(2, 40); j++) {
+                    enrollmentRepository.save(new Enrollment(member, sub, 100, true));
+                    Post post = new Post( memberUsername,
+                            faker.lorem().sentence(),
+                            String.join(" ", faker.lorem().sentences(faker.number().numberBetween(1, 10))), faker.number().numberBetween(-5, 100),
+                            null, sub);
+                    post.setCreated(faker.date().past(12 * 30, TimeUnit.DAYS));
+                    postRepository.save(post);
+                }
+
+                generatedSubClubs.add(sub);
             }
         }
 
         for(int i = 1; i <= 5; i++){
-            enrollmentRepository.save(new Enrollment(memberRepository.findByUsername(adminList.get(i - 1)), subClubRepository.findByName("sub" + i).get(), 100, true));
-            enrollmentRepository.save(new Enrollment(memberRepository.findByUsername("member" + i), subClubRepository.findByName("sub" + i).get(), 100, true));
-            SubClub subClub = subClubRepository.findByName("sub" + i).get();
+            SubClub subClub = generatedSubClubs.get(i - 1);
+            enrollmentRepository.save(new Enrollment(memberRepository.findByUsername(adminList.get(i - 1)), subClub, 100, true));
             subClub.setModerator(memberRepository.findByUsername(adminList.get(i-1)));
             subClubRepository.save(subClub);
         }
