c7d153e4b20e04e9b0d0f6ea673fde029d9dc00a
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-05-25 17:19:24 +0300

    Fix CORS error for PUT and DELETE requests

--- src/main/java/com/overengineers/cospace/config/WebSecurityConfiguration.java
+++ src/main/java/com/overengineers/cospace/config/WebSecurityConfiguration.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,6 +23,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @CrossOrigin
 @Configuration
@@ -78,7 +83,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        CorsConfiguration allowMethods = new CorsConfiguration();
+        allowMethods.applyPermitDefaultValues();
+        allowMethods.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE"));
+        source.registerCorsConfiguration("/**", allowMethods);
         return source;
     }
 }
